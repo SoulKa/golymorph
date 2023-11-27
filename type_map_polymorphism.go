5d5b0b083cc521f0119c532442a1433812ed9823
SoulKa <louiswilke@gmx.de>
2023-11-27 22:37:02 +0100

    file renaming

--- type_map_polymorphism.go
+++ type_map_polymorphism.go
@@ -7,23 +7,6 @@ import (
 	"reflect"
 )
 
-type TypeMap map[any]reflect.Type
-
-type Polymorpher interface {
-	// AssignTargetType assigns the determined type to target based on the polymorphism rules. The matching rule with the
-	// highest priority is used. If no rule matches, the target type is not changed. The source and target must be pointers.
-	AssignTargetType(source any, target any) (error, bool)
-}
-
-// RulePolymorphism is a mapper that assigns a target type based on the given rules
-type RulePolymorphism struct {
-	// targetPath is the path to the object to assign the new type to
-	targetPath objectpath.ObjectPath
-
-	// rules is a list of rules to apply. The first rule that matches is used to determine the target type.
-	rules []Rule
-}
-
 // TypeMapPolymorphism is a mapper that assigns a target type based on a discriminator value and a type map
 type TypeMapPolymorphism struct {
 	// targetPath is the path to the object to assign the new type to
@@ -34,23 +17,6 @@ type TypeMapPolymorphism struct {
 
 	// typeMap is a map of discriminator values to types
 	typeMap TypeMap
-}
-
-func (p *RulePolymorphism) AssignTargetType(source any, target any) (error, bool) {
-
-	// check for each rule if it matches and assign type if it does
-	for _, rule := range p.rules {
-		if err, matches := rule.Matches(source); err != nil {
-			return errors.Join(errors.New("error applying rule"), err), false
-		} else if matches {
-			if err := objectpath.AssignTypeAtPath(target, p.targetPath, rule.NewType); err != nil {
-				return errors.Join(errors.New("error assigning type to target"), err), false
-			}
-			return nil, true
-		}
-	}
-	return nil, false
-
 }
 
 func (p *TypeMapPolymorphism) AssignTargetType(source any, target any) (error, bool) {
