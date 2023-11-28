012bb3bddfc13c5959932b93cd98cb913c413105
SoulKa <louiswilke@gmx.de>
2023-11-28 14:47:53 +0100

    update docs

--- polymorphism_builder.go
+++ polymorphism_builder.go
@@ -11,27 +11,43 @@ type polymorphismBuilderBase struct {
 }
 
 type polymorphismBuilderEmpty interface {
+	// DefineTypeAt defines the target path of the polymorphism. This is the path where the polymorphism
+	// will be applied, i.e. where the new type is set. For valid paths see objectpath.NewObjectPathFromString.
 	DefineTypeAt(targetPath string) polymorphismBuilderStrategySelector
 }
 
 type polymorphismBuilderStrategySelector interface {
+	// UsingRule defines a rule that is used to determine the new type. The rules are applied in the
+	// order they are defined. The first rule that matches is used to determine the new type.
 	UsingRule(rule Rule) polymorphismBuilderRuleAdder
+
+	// UsingTypeMap defines a type map that is used to determine the new type. The type map is applied
 	UsingTypeMap(typeMap TypeMap) polymorphismBuilderDiscriminatorKeyDefiner
 }
 
 type polymorphismBuilderRuleAdder interface {
+	// UsingRule defines a rule that is used to determine the new type. The rules are applied in the
+	// order they are defined. The first rule that matches is used to determine the new type.
 	UsingRule(rule Rule) polymorphismBuilderRuleAdder
+
+	// Build creates a new TypeResolver that can be used to resolve a polymorphic type.
 	Build() (error, TypeResolver)
 }
 
 type polymorphismBuilderDiscriminatorKeyDefiner interface {
+	// WithDiscriminatorAt defines the path to the discriminator key. The discriminator key is used to
+	// determine the new type. The value of the discriminator key is used to lookup the new type in the
+	// type map.
 	WithDiscriminatorAt(discriminatorKey string) polymorphismBuilderFinalizer
 }
 
 type polymorphismBuilderFinalizer interface {
+	// Build creates a new TypeResolver that can be used to resolve a polymorphic type.
 	Build() (error, TypeResolver)
 }
 
+// NewPolymorphismBuilder creates a new polymorphism builder that is used in a human readable way to create a polymorphism.
+// It only allows a valid combination of rules and type maps.
 func NewPolymorphismBuilder() polymorphismBuilderEmpty {
 	return &polymorphismBuilderBase{*objectpath.NewSelfReferencePath(), []error{}}
 }
--- rule_builder.go
+++ rule_builder.go
@@ -13,20 +13,25 @@ type ruleBuilder struct {
 }
 
 type ruleBuilderBase interface {
-	WhenValueAt(valuePath objectpath.ObjectPath) ruleBuilderConditionSetter
-	WhenValueAtPathString(valuePath string) ruleBuilderConditionSetter
+	// WhenValueAt sets the path to the value in the source to compare.
+	WhenValueAt(valuePath string) ruleBuilderConditionSetter
 }
 
 type ruleBuilderConditionSetter interface {
+	// IsEqualTo sets the value to compare to.
 	IsEqualTo(value any) ruleBuilderTypeAssigner
+
+	// Matches sets the function to use to compare the value at ValuePath to.
 	Matches(comparator func(any) bool) ruleBuilderTypeAssigner
 }
 
 type ruleBuilderTypeAssigner interface {
+	// ThenAssignType sets the type to assign to the target if the rule matches.
 	ThenAssignType(newType reflect.Type) ruleBuilderFinalizer
 }
 
 type ruleBuilderFinalizer interface {
+	// Build builds the Rule and returns the errors encountered while building.
 	Build() ([]error, Rule)
 }
 
@@ -35,14 +40,7 @@ func NewRuleBuilder() ruleBuilderBase {
 	return &ruleBuilder{}
 }
 
-// WhenValueAt sets the path to the value in the source to compare.
-func (b *ruleBuilder) WhenValueAt(valuePath objectpath.ObjectPath) ruleBuilderConditionSetter {
-	b.valuePath = valuePath
-	return b
-}
-
-// WhenValueAtPathString sets the path to the value in the source to compare.
-func (b *ruleBuilder) WhenValueAtPathString(valuePath string) ruleBuilderConditionSetter {
+func (b *ruleBuilder) WhenValueAt(valuePath string) ruleBuilderConditionSetter {
 	if err, path := objectpath.NewObjectPathFromString(valuePath); err != nil {
 		b.appendError(err)
 	} else {
@@ -51,25 +49,21 @@ func (b *ruleBuilder) WhenValueAtPathString(valuePath string) ruleBuilderConditi
 	return b
 }
 
-// IsEqualTo sets the value to compare to.
 func (b *ruleBuilder) IsEqualTo(value any) ruleBuilderTypeAssigner {
 	b.comparatorFunc = func(v any) bool { return v == value }
 	return b
 }
 
-// Matches sets the function to use to compare the value at ValuePath to.
 func (b *ruleBuilder) Matches(comparator func(any) bool) ruleBuilderTypeAssigner {
 	b.comparatorFunc = comparator
 	return b
 }
 
-// ThenAssignType sets the type to assign to the target if the rule matches.
 func (b *ruleBuilder) ThenAssignType(newType reflect.Type) ruleBuilderFinalizer {
 	b.newType = newType
 	return b
 }
 
-// Build builds the Rule and returns the errors encountered while building.
 func (b *ruleBuilder) Build() ([]error, Rule) {
 	return b.errors, Rule{
 		b.valuePath,
